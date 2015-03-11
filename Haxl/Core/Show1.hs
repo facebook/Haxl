9f873ed35a3d78653b0231761e320e909938352e
Zejun Wu <watashi@fb.com>
2015-03-11 10:06:07 -0700

    Update haxl copyright headers
    Summary:
    Update haxl copyright headers: s/2014/2014-present/g
    Thank @siddharthasharma for pointing this out
    Closes #26, #27
    
    Test Plan: meh
    
    Reviewed By: jon.coens@fb.com
    
    Subscribers: ldbrandy, memo, watashi, smarlow, akr, bnitka, jcoens
    
    FB internal diff: D1903841
    
    Signature: t1:1903841:1426073797:b95462075265fa8ec9c1252e45ef5cac7a37e7e2

--- Haxl/Core/Show1.hs
+++ Haxl/Core/Show1.hs
@@ -1,4 +1,4 @@
--- Copyright (c) 2014, Facebook, Inc.
+-- Copyright (c) 2014-present, Facebook, Inc.
 -- All rights reserved.
 --
 -- This source code is distributed under the terms of a BSD license,
