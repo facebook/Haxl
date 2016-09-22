78052b96256f745221b053faef75f1d226797cc1
Jonathan Coens <jon.coens@fb.com>
2016-09-22 11:03:58 -0700

    Derive NFData instance for GenHaxl
    Summary: Having an NFData instance allows forcing calls that return a Haxl computation to run.
    
    Reviewed By: xich
    
    Differential Revision: D3906781
    
    fbshipit-source-id: e49448152695383031af6d315a20dbcd1c10720c

--- Haxl/Core/Monad.hs
+++ Haxl/Core/Monad.hs
@@ -11,6 +11,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE MultiWayIf #-}
@@ -182,6 +183,7 @@ emptyEnv = initEnv stateEmpty
 --
 newtype GenHaxl u a = GenHaxl
   { unHaxl :: Env u -> IORef (RequestStore u) -> IO (Result u a) }
+  deriving NFData
 
 -- | The result of a computation is either 'Done' with a value, 'Throw'
 -- with an exception, or 'Blocked' on the result of a data fetch with
