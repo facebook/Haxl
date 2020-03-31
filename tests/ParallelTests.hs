6b0734e022db6a5141ede4fe9b08cb82222ea253
Josef Svenningsson <josefs@fb.com>
2020-03-31 08:18:54 -0700

    Test that pOr has the right semantics in the presence of blocking
    Summary:
    The standard semantics test only tests the easy case when there is no blocking involved.
    
    Also, change `sync_test` so that the data fetches are not cached. That confused me quite a lot during a debugging session.
    
    Reviewed By: simonmar
    
    Differential Revision: D19142099
    
    fbshipit-source-id: 89697dbb896a1696aa916e3fcf659bf6a031f076

--- tests/ParallelTests.hs
+++ tests/ParallelTests.hs
@@ -22,10 +22,27 @@ sync_test = do
   -- without causing an error. The reason we test for this is that the
   -- synchronization involved in this case is a little fragile.
   False <- runHaxl env $ do
-    (fmap (const False) (sleep 100)
+    (fmap (const False) (sleep 50)
       `pOr` fmap (const False) (sleep 100))
       `pOr` fmap (const False) (sleep 200)
   return ()
+
+semantics_when_computation_is_blocked_test :: IO ()
+semantics_when_computation_is_blocked_test = do
+  env <- testEnv
+    -- Test semantics of blocking
+  let sleepReturn bool t = do
+        _ <- sleep t
+        return bool
+  r <- runHaxl env $ do
+        -- All sleep times are different so that they're not cached
+        a <- sleepReturn False 10 `pOr` sleepReturn False 11
+        b <- sleepReturn False 12 `pOr` sleepReturn True 13
+        c <- sleepReturn True 14 `pOr` sleepReturn False 15
+        d <- sleepReturn True 16 `pOr` sleepReturn True 17
+        return (not a && b && c && d)
+  assertBool "pOr blocked semantics" r
+
 
 timing_test = do
   env <- testEnv
@@ -51,4 +68,5 @@ timing_test = do
 
 tests = TestList [TestLabel "sync_test" (TestCase sync_test)
                  ,TestLabel "timing_test" (TestCase timing_test)
+                 ,TestLabel "semantics_when_computation_is_blocked_test" (TestCase semantics_when_computation_is_blocked_test)
                  ]
