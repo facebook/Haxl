3e42e870c971e471b89b306a98eb39deacd7833b
Dylan Yudaken <dylany@fb.com>
2020-04-24 03:15:42 -0700

    clean up monadbench a bit
    Summary:
    * add ability to set reporting flag for benchmarking
    * add argument parser
    * add a test for using lots of labels
    
    Reviewed By: simonmar
    
    Differential Revision: D20792436
    
    fbshipit-source-id: a63603a6cbcbe48870bfd4f3c7f7784e59e0d969

--- tests/MonadBench.hs
+++ tests/MonadBench.hs
@@ -5,13 +5,15 @@
 -- found in the LICENSE file.
 
 -- | Benchmarking tool for core performance characteristics of the Haxl monad.
-{-# LANGUAGE CPP, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ApplicativeDo, RecordWildCards #-}
 module MonadBench (main) where
 
 import Control.Monad
 import Data.List as List
+import Data.Maybe
 import Data.Time.Clock
-import System.Environment
+import Options.Applicative
 import System.Exit
 import System.IO
 import Text.Printf
@@ -22,114 +24,136 @@ import Prelude()
 import Haxl.Core.Memo (newMemoWith, runMemo)
 
 import Haxl.Core
+import Haxl.Core.Util
 
 import ExampleDataSource
 
 newtype SimpleWrite = SimpleWrite Text deriving (Eq, Show)
 
-testEnv :: IO (Env () SimpleWrite)
-testEnv = do
+testEnv :: Int -> IO (Env () SimpleWrite)
+testEnv report = do
   exstate <- ExampleDataSource.initGlobalState
   let st = stateSet exstate stateEmpty
-  initEnv st ()
+  env <- initEnv st ()
+  return env { flags = (flags env) { report = report } }
 
-main :: IO ()
-main = do
-  [test,n_] <- getArgs
-  let n = read n_
-  env <- testEnv
-  t0 <- getCurrentTime
-  case test of
+type Test = (String, Int, Int -> GenHaxl () SimpleWrite ())
+
+testName :: Test -> String
+testName (t,_,_) = t
+
+allTests :: [Test]
+allTests =
     -- parallel, identical queries
-    "par1" -> runHaxl env $
-       Haxl.sequence_ (replicate n (listWombats 3))
+  [ ("par1", large, \n -> Haxl.sequence_ (replicate n (listWombats 3)))
     -- parallel, distinct queries
-    "par2" -> runHaxl env $
-       Haxl.sequence_ (map listWombats [1..fromIntegral n])
+  , ("par2", medium, \n ->
+        Haxl.sequence_ (map listWombats [1..fromIntegral n]))
     -- sequential, identical queries
-    "seqr" -> runHaxl env $
-       foldr andThen (return ()) (replicate n (listWombats 3))
+  , ("seqr", huge, \n ->
+        foldr andThen (return ()) (replicate n (listWombats 3)))
     -- sequential, left-associated, distinct queries
-    "seql" -> runHaxl env $ do
+  , ("seql", medium, \n -> do
        _ <- foldl andThen (return []) (map listWombats [1.. fromIntegral n])
-       return ()
-    "tree" -> runHaxl env $ void $ tree n
+       return ())
     -- No memoization
-    "memo0" -> runHaxl env $
-      Haxl.sequence_ [unionWombats | _ <- [1..n]]
+  , ("memo0", small, \n -> Haxl.sequence_ [unionWombats | _ <- [1..n]])
     -- One put, N gets.
-    "memo1" -> runHaxl env $
-      Haxl.sequence_ [memo (42 :: Int) unionWombats | _ <- [1..n]]
+  , ("memo1", medium_large, \n ->
+        Haxl.sequence_ [memo (42 :: Int) unionWombats | _ <- [1..n]])
     -- N puts, N gets.
-    "memo2" -> runHaxl env $
-      Haxl.sequence_ [memo (i :: Int) unionWombats | i <- [1..n]]
-    "memo3" ->
-      runHaxl env $ do
+  , ("memo2", small, \n ->
+        Haxl.sequence_ [memo (i :: Int) unionWombats | i <- [1..n]])
+  , ("memo3", medium_large, \n -> do
         ref <- newMemoWith unionWombats
         let c = runMemo ref
-        Haxl.sequence_ [c | _ <- [1..n]]
-    "memo4" ->
-      runHaxl env $ do
+        Haxl.sequence_ [c | _ <- [1..n]])
+  , ("memo4", small, \n -> do
         let f = unionWombatsTo
-        Haxl.sequence_ [f x | x <- take n $ cycle [100, 200 .. 1000]]
-    "memo5" ->
-      runHaxl env $ do
+        Haxl.sequence_ [f x | x <- take n $ cycle [100, 200 .. 1000]])
+  , ("memo5", medium_large, \n -> do
         f <- memoize1 unionWombatsTo
-        Haxl.sequence_ [f x | x <- take n $ cycle [100, 200 .. 1000]]
-    "memo6" ->
-      runHaxl env $ do
+        Haxl.sequence_ [f x | x <- take n $ cycle [100, 200 .. 1000]])
+  , ("memo6", small, \n -> do
         let f = unionWombatsFromTo
         Haxl.sequence_ [ f x y
                        | x <- take n $ cycle [100, 200 .. 1000]
                        , let y = x + 1000
-                       ]
-    "memo7" ->
-      runHaxl env $ do
+                       ])
+  , ("memo7", medium_large, \n -> do
         f <- memoize2 unionWombatsFromTo
         Haxl.sequence_ [ f x y
                        | x <- take n $ cycle [100, 200 .. 1000]
                        , let y = x + 1000
-                       ]
+                       ])
+  , ("cc1", medium_large, \n ->
+        Haxl.sequence_ [ cachedComputation (ListWombats 1000) unionWombats
+                       | _ <- [1..n]
+                       ])
+  , ("tree", 20, \n -> void $ tree n (\_ act -> act))
+  , ("tree_labels", 20, \n -> void $
+      tree n (\n act -> withLabel (textShow n) act))
     -- parallel writes
-    "write1" -> runHaxl env $
-      Haxl.sequence_ (replicate n (tellWrite (SimpleWrite "haha")))
-
+  , ("write1", large, \n ->
+        Haxl.sequence_ (replicate n (tellWrite (SimpleWrite "haha"))))
     -- sequential writes
-    "write2" -> runHaxl env $
-      foldr andThen (return ()) (replicate n (tellWrite (SimpleWrite "haha")))
+  , ("write2", huge, \n -> foldr
+        andThen
+        (return ())
+        (replicate n (tellWrite (SimpleWrite "haha"))))
+  ]
+  where
+    huge = large * 10
+    large =  medium * 10
+    medium_large =  medium * 4
+    medium = 200000
+    small = 1000
 
+data Options = Options
+  { test :: String
+  , nOverride :: Maybe Int
+  , reportFlag :: Int
+  }
 
-    "cc1" -> runHaxl env $
-      Haxl.sequence_ [ cachedComputation (ListWombats 1000) unionWombats
-                     | _ <- [1..n]
-                     ]
-
-    _ -> do
-      hPutStrLn stderr $ "syntax: monadbench " ++ concat
-        [ "par1"
-        , "par2"
-        , "seqr"
-        , "seql"
-        , "memo0"
-        , "memo1"
-        , "memo2"
-        , "memo3"
-        , "memo4"
-        , "memo5"
-        , "memo6"
-        , "memo7"
-        , "cc1"
-        ]
-      exitWith (ExitFailure 1)
+runTest :: Options -> Test -> IO ()
+runTest Options{..} (t, nDef, act) = do
+  let n = fromMaybe nDef nOverride
+  env <- testEnv reportFlag
+  t0 <- getCurrentTime
+  runHaxl env $ act n
   t1 <- getCurrentTime
-  printf "%10s: %10d reqs: %.2fs\n"
-    test n (realToFrac (t1 `diffUTCTime` t0) :: Double)
+  printf "%12s: %10d reqs: %.2fs\n"
+    t n (realToFrac (t1 `diffUTCTime` t0) :: Double)
 
-tree :: Int -> GenHaxl () SimpleWrite [Id]
-tree 0 = listWombats 0
-tree n = concat <$> Haxl.sequence
-  [ tree (n-1)
-  , listWombats (fromIntegral n), tree (n-1)
+optionsParser :: Parser Options
+optionsParser = do
+  test <- argument str (metavar "TEST")
+  reportFlag <- option auto (long "report" <> value 0 <> metavar "REPORT")
+  nOverride <- optional $ argument auto (metavar "NUM")
+  return Options{..}
+
+main :: IO ()
+main = do
+  opts@Options{..} <- execParser $ info optionsParser mempty
+  let tests = if test == "all"
+        then allTests
+        else filter ((==) test . testName) allTests
+  when
+    (null tests)
+    (do
+        hPutStrLn stderr $ "syntax: monadbench [all|" ++
+          intercalate "|" (map testName allTests) ++ "]"
+        exitWith (ExitFailure 1))
+  Control.Monad.mapM_ (runTest opts) tests
+
+tree
+  :: Int
+  -> (Int -> GenHaxl () SimpleWrite [Id] -> GenHaxl () SimpleWrite [Id])
+  -> GenHaxl () SimpleWrite [Id]
+tree 0 wrap = wrap 0 $ listWombats 0
+tree n wrap = wrap n $ concat <$> Haxl.sequence
+  [ tree (n-1) wrap
+  , listWombats (fromIntegral n), tree (n-1) wrap
   ]
 
 unionWombats :: GenHaxl () SimpleWrite [Id]
